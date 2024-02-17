0c290d852e35aefeea71fb76d92f0c61b79f7889
Marco Baldo <bldmrc92@gmail.com>
2024-02-17 19:18:18 +0100

    My first try

---
+++ Untitled-1.js
@@ -0,0 +1,5 @@
+console.log("__________________________")
+console.log("RISE and Shine!")
+console.log("Ready for a new day!")
+console.log("__________________________")
+console.log("__________________________")
