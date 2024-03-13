9cde9432f0e31af3fc0cc417f9fcf5af88dac305
Marco Baldo <baldo@fld.czu.cz>
2024-03-13 14:52:56 +0100

    Was a try only

--- Untitled-1.js
+++
@@ -1,6 +0,0 @@
-console.log("__________________________")
-console.log("RISE and Shine!")
-console.log("Ready for a new day!")
-console.log("__________________________")
-console.log("__________________________")
-
